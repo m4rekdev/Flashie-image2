37945fe78ace18fef8f62c2b3effe824b8090e66
goodbyepavlyi goodbyepavlyi <pavlyi.off@gmail.com>
2021-11-14 21:28:05 +0100

    TikTok v2

--- events/ready.js
+++ events/ready.js
@@ -1,14 +1,14 @@
 const { instagram_username, instagram_password, twitter_key, twitter_secret, twitter_token, tiktok_username, tiktok_password } = require('../config.json');
 const instagramApi = require('user-instagram');
 const twitterApi = require('twitter');
-const tiktokApi = require('tiktok-app-api');
+const tiktok = require('tiktok-app-api');
 
 const twitterClient = new twitterApi({
     consumer_key: twitter_key,
     consumer_secret: twitter_secret,
     bearer_token: twitter_token
 });
-const tiktokClient = new tiktokApi();
+const tiktokApp = new tiktok();
 
 
 module.exports = {
@@ -16,7 +16,8 @@ module.exports = {
     once: true,
     async execute(client) {
         console.log(`${client.user.tag} is now online!`);
-
+const user = tiktokApp.getUserByID('6828402025898902533');
+console.log(user);
         console.log("disablnuty login na instagram v ready.js:15")
         // await instagramApi.authenticate(instagram_username, instagram_password);
     },
@@ -24,4 +25,4 @@ module.exports = {
 
 module.exports.instagramApi = instagramApi;
 module.exports.twitterClient = twitterClient;
-module.exports.tiktokClient = tiktokClient;
+// module.exports.tiktokClient = tiktokClient;
