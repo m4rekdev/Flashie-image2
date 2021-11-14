b6c03b8884b3bfb8a5a538e02d03cdb41053457e
goodbyepavlyi goodbyepavlyi <pavlyi.off@gmail.com>
2021-11-14 10:32:58 +0100

    Commit

--- handlers/twitter.js
+++ handlers/twitter.js
@@ -1,17 +1,10 @@
 const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
-const cache = {};
 
 module.exports = {
     async get(username) {
         let data = {};
-        const url = "https://mobile.twitter.com/" + username;
-        const response = request(url, (_, __, body) => {
-            const $ = cheerio.load(body);
-            const url = ($(".avatar img").attr("src") || "").replace("_normal", size);
-            cache[username] = url;
-            console.log(url);
-        });
-        // console.log(response);
+        
+
 
         return data;
     }
