bb02e9bf483e0ffd709b2cd67fe87db09165ea9b
goodbyepavlyi goodbyepavlyi <pavlyi.off@gmail.com>
2021-11-17 12:12:42 +0100

    Master

--- commands/status.js
+++ commands/status.js
@@ -6,12 +6,17 @@ const formatDuration = require('../handlers/misc/formatDuration');
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('status')
-        .setDescription('Shows the bot\'s ping.'),
+        .setDescription('Shows the bot\'s status.'),
     async execute(interaction) {
         const embed = new MessageEmbed()
             .setColor('#d37d63')
             .setTitle('Status')
-            .setDescription(`**🏓 __Ping__**\n> Latency: ${Math.abs(Date.now() - interaction.createdTimestamp)}ms\n> API Latency: ${Math.round(interaction.client.ws.ping)}ms\n\n**🕛 __Uptime__**\n> ${await formatDuration.format(interaction.client.uptime)}`)
+            .setDescription(`🏓 **Ping**
+            > Latency: ${Date.now() - interaction.createdTimestamp}ms
+            > API Latency: ${Math.round(interaction.client.ws.ping)}ms
+            
+            🕛 __Uptime__ 🕛
+            > ${await formatDuration.format(interaction.client.uptime)}`)
             .setFooter(`Ran by ${interaction.member.user.tag}`, interaction.member.user.displayAvatarURL());
         await interaction.reply({embeds: [embed]});
     },
